a38569fa355fb65890dc7a5c5df59dd89894ce80
Benz <benje@hotmail.ca>
2016-11-11 19:25:51 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ben
+ * Date: 11/11/2016
+ * Time: 3:12 PM
+ */
+require_once "controller.php";
+require_once "behavior.php";
+require_once "userStuff/userController.php";
+
+$c = new myController();
+?>
